e21bd251b89e437d3b8be09f6088f9ef3585d799
Saepudin <saep@ymail.com>
2026-01-28 09:07:41 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,14 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabaseUrl = 'https://aedtrwpomswdqxarvsrg.supabase.co';
 export const supabaseKey = 'sb_publishable_QXwMaSw5_11T2BpcWetm2A_hTgZeAsf';
-
-// Named export supabase agar bisa dipakai di file lain
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Tambahkan settingan Cloudinary di sini
+export const cloudinaryConfig = {
+    cloudName: 'dmm6avtxd',
+    uploadPreset: 'robopanda-preset',
+    // URL otomatis mengikuti cloudName di atas
+    get uploadUrl() {
+        return `https://api.cloudinary.com/v1_1/${this.cloudName}/image/upload`;
+    }
+};
